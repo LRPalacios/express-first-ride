9ec80b21af14ec56c7948b2fe6cd8b012516f375
LRPalacios <lrpalaciosdev@gmail.com>
2017-05-07 22:33:15 -0600

    add handle bars as template engine

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var express = require('express');
-
+var handlebars = require('express-handlebars');
 var app = express();
 
 app.use(express.static('public'));
 app.set('views', 'src/views');
-app.set('view engine', 'jade');
+
+app.engine('.hbs', handlebars({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
 
 app.get('/', function (req, res) {
     res.render('index', {
+        title: 'Hello from render',
         list: ['1', '2', '3']
     });
 });
